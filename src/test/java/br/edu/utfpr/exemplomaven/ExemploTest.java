1c89d1603c4092c5ef48c46120353b0b4d18424b
rickmussi <rickmussi@hotmail.com>
2018-06-16 18:46:06 -0300

    Ricardo Mussi
    Implementado 3 casos de teste para o site https://ration.io

--- src/test/java/br/edu/utfpr/exemplomaven/ExemploTest.java
+++ src/test/java/br/edu/utfpr/exemplomaven/ExemploTest.java
@@ -1,6 +1,5 @@
 package br.edu.utfpr.exemplomaven;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -10,6 +9,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -32,17 +32,17 @@ public class ExemploTest {
      *
      * Versão utilizada do chromedriver: 2.35.528139
      */
-    private static String CHROMEDRIVER_LOCATION = "/home/utfpr/install/selenium/chromedriver";
-    
+    private static String CHROMEDRIVER_LOCATION = "c:\\wcd\\TestadorAPP\\chromedriver.exe";
+
     private static int scId = 0;
 
     WebDriver driver;
-    
+
     @BeforeClass
     public static void beforeClass() {
         System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_LOCATION);
     }
-    
+
     @Before
     public void before() {
         ChromeOptions chromeOptions = new ChromeOptions();
@@ -50,39 +50,76 @@ public class ExemploTest {
         chromeOptions.addArguments("headless");
         chromeOptions.addArguments("window-size=1200x600");
         chromeOptions.addArguments("start-maximized");
-        
+
         driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
-    
+
     @After
     public void after() {
         driver.close();
     }
-    
+
+    @Ignore
     @Test
     public void testGoogleSearch() {
         driver.get("https://www.google.com.br/");
         WebElement searchInput = driver.findElement(By.name("q"));
         searchInput.sendKeys("teste de software");
-        
+
         takeScreenShot();
-        
+
         searchInput.submit();
-        
+
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until( (ExpectedCondition<Boolean>) (WebDriver d) -> d.getTitle().toLowerCase().startsWith("teste") );
-        
+        wait.until((ExpectedCondition<Boolean>) (WebDriver d) -> d.getTitle().toLowerCase().startsWith("teste"));
+
         takeScreenShot();
-        
+
         assertTrue(driver.getTitle().startsWith("teste de software"));
     }
+
+    @Test
+    public void testRationSearch() {
+        driver.get("https://ration.io");
+        WebElement searchSign = driver.findElement(By.xpath("//*[@id=\"page-header\"]/div/a[2]"));
+        searchSign.click();
+
+        WebElement searchEmail = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/form/div[1]/input"));
+        searchEmail.sendKeys("rickmussi@hotmail.com");
+
+        WebElement searchSenha = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/form/div[2]/input"));
+        searchSenha.sendKeys("123");
+
+        WebElement searchBtnSign = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/form/div[4]/button"));
+        searchBtnSign.click();
+                                                                
+        WebElement searchRetorno = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/form/p/small"));
+        assertEquals(searchRetorno.getText(), "The credentials you entered are not associated with an account. Please check your email and/or password and try again.");
+    }
+      
+    @Test
+    public void testFaq() {
+        driver.get("https://ration.io");
+        WebElement searchFaq = driver.findElement(By.xpath("//*[@id=\"page-header\"]/div/a[1]"));
+        searchFaq.click();        
+        assertEquals(driver.getCurrentUrl(), "https://ration.io/faq");
+    }
     
+    @Test
+    public void testRationTitulo() {
+        driver.get("https://ration.io");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        assertTrue(driver.getTitle().equals("Ration"));
+    }
+
     private void takeScreenShot() {
         File sourceFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         try {
             scId++;
             FileUtils.copyFile(sourceFile, new File("./res/" + scId + ".png"));
-        } catch(IOException e) {}
+        } catch (IOException e) {
+        }
     }
 }
